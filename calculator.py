a95effe3ba735c1b6743cb9df29e343f79a7c06a
Morshed-19-39639-1 <78311288+Morshed-19-39639-1@users.noreply.github.com>
2024-09-01 07:52:52 +0600

    Updated

--- calculator.py
+++ calculator.py
@@ -7,21 +7,21 @@ print("----press keys for operator (+,-,*,/,%,//,** )----------")
 operator = input("Enter operator  : ")
 
 if operator == "+":
-   print("The sum is : ",first + second)
-elif operator == "-":
-   print("The subtraction is : ",first - second)
-elif operator == "*":
-   print("The multiplication is : ",first * second)
-elif operator == "**":
-   print("The powered value is : ",first ** second)# powered value
-elif operator == "/":
-   print("The divide is : ",first / second)
-elif operator == "//":
-   print("The floor value is : ",first // second)# floored value
-elif operator == "%":
-   print("The reminder is : ",first % second)
-else:
-   print("Invalid Operation")
+      print("The sum is : ",first + second)
+   elif operator == "-":
+      print("The subtraction is : ",first - second)
+   elif operator == "*":
+      print("The multiplication is : ",first * second)
+   elif operator == "**":
+      print("The powered value is : ",first ** second)# powered value
+   elif operator == "/":
+      print("The divide is : ",first / second)
+   elif operator == "//":
+      print("The floor value is : ",first // second)# floored value
+   elif operator == "%":
+      print("The reminder is : ",first % second)
+   else:
+      print("Invalid Operation")
 
 
 
